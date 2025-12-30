2bd3d9522e34f780a7f0ec104ab2b3fc2534878c
katticat <107314014+katticat@users.noreply.github.com>
2025-12-30 17:40:36 +0100

    Create sachbuecher.js

---
+++ functions/feed/sachbuecher.js
@@ -0,0 +1,24 @@
+export async function onRequestGet() {
+  const FEED_URL = "https://www.bibkat.de/buecherei-obernau/medialist/sachbucher-fur-erwachsene/";
+
+  const upstream = await fetch(FEED_URL, {
+    headers: { "User-Agent": "BuechereiObernau-FeedProxy/1.0" },
+  });
+
+  if (!upstream.ok) {
+    return new Response(`Upstream error: ${upstream.status}`, { status: 502 });
+  }
+
+  let xml = await upstream.text();
+
+  // Entfernt eine evtl. vorhandene xml-stylesheet Verarbeitungsvorschrift
+  xml = xml.replace(/<\?xml-stylesheet[^>]*\?>\s*/i, "");
+
+  return new Response(xml, {
+    headers: {
+      "content-type": "application/xml; charset=utf-8",
+      "cache-control": "public, max-age=600",
+      "access-control-allow-origin": "*",
+    },
+  });
+}
